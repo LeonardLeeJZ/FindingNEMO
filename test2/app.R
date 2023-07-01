416661ed5527e0cfe2440ed6c6ecc707883d1613
haileycsy <hailey.cheong.sy@gmail.com>
2023-07-02 02:54:20 +0800

    Commit after adding working test2

---
+++ test2/app.R
@@ -0,0 +1,105 @@
+pacman::p_load(igraph, tidygraph, ggraph, 
+               visNetwork, lubridate, clock,
+               tidyverse, graphlayouts, bslib)
+
+# Read the data
+nodes <- read_csv("data/mc3_shinynodes.csv")
+links <- read_csv("data/mc3_links_new.csv")
+
+ui <- fluidPage(
+  titlePanel(title = "Similar Groups"),
+  sidebarLayout(
+    sidebarPanel = sidebarPanel(
+      selectInput(
+        inputId = "industry",
+        label = "Select Industry:",
+        choices = c(
+          `Fishing-related` = "Fishing-related Company",
+          `Industrial` = "Industrial Company",
+          `Food-related` = "Food Company",
+          `Seafood Processing` = "Seafood-processing Company",
+          `Consumer Goods` = "Consumer-goods Company",
+          `Transport & Logistics` = "Transport-logistics Company",
+          `Multi-industry` = "Multi-Industry Company"
+          
+        ),
+        selected = "Fishing-related Company"
+      )
+    ),
+    mainPanel = mainPanel(
+      title = "Industry-based Networks",
+      plotOutput("networkPlot", height = "800px", width = "800px")
+    )
+  ),
+  theme = bs_theme(bootswatch = "morph")
+)
+
+server <- function(input, output) {
+  
+  output$networkPlot <- renderPlot({
+    
+    # Filter nodes data from input$industry
+    filtered_nodes <- nodes %>%
+      filter(group == input$industry)
+    
+    # Filter links based filtered_nodes to get connections
+    filtered_links <- links %>%
+      filter(source %in% filtered_nodes$id)
+    
+    # Get unique source and target
+    links_source <- filtered_links %>%
+      distinct(source) %>%
+      rename("id" = "source")
+    
+    links_target <- filtered_links %>%
+      distinct(target) %>%
+      rename("id" = "target")
+    
+    # bind links to get overall nodes dataframe
+    filtered_nodes_new <- bind_rows(links_source, links_target) %>%
+      left_join(nodes, by = "id") %>%
+      select(id, group)
+    
+    # Create graph object
+    filtered_graph <- tbl_graph(nodes = filtered_nodes_new,
+                                  edges = filtered_links, 
+                                  directed = FALSE)
+    
+    # Define Similarity Measure
+    V(filtered_graph)$degree <- degree(filtered_graph, mode = "all")
+    
+    set.seed(1234)
+    ggraph(filtered_graph,
+           layout = "nicely"
+      ) +
+      geom_edge_fan(
+        alpha = .6,
+        show.legend = FALSE
+      ) +
+      scale_edge_width(
+        range = c(0.1,4)
+      ) +
+      geom_node_point(
+        aes(size = degree,
+            color = group),
+        alpha = .9
+      ) +
+      # Remove the legend for "degree"
+      guides(color = guide_legend(title = "Role:"),
+             size = "none"
+      ) + 
+      geom_node_text(
+        aes(label = ifelse(degree > quantile(degree, .75), id, "")), 
+        size = 2,
+        repel = TRUE
+      ) +
+      theme(
+        plot.title = element_text(size = 16,
+                                  color = "grey20"),
+        legend.title = element_text()
+      )
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
